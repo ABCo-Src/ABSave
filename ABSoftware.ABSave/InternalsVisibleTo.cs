cbfb618f5a20e5dd7f863c76fa656093df79d9f2
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-11 12:45:11 +0100

    Fix building

--- ABSoftware.ABSave/InternalsVisibleTo.cs
+++ ABSoftware.ABSave/InternalsVisibleTo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("ABSoftware.ABSave.UnitTests,PublicKey=" +
+[assembly: InternalsVisibleTo("ABCo.ABSave.UnitTests,PublicKey=" +
     "0024000004800000940000000602000000240000525341310004000001000100b5f7ee33f51a2b" +
     "1770ddc0d7c04adde37382b11e83cecc46d60f9f7bac5492fad71d066f760e2b163f100aa2a51f" +
     "036843f13de106ee3dd3c38a8c6ea28dfcf6d08e0a633d2c69bdace83858240697c03de97def5d" +
