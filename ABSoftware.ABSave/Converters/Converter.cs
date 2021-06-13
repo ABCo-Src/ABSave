87115bcc689bf158bcbf9a838715cda50765828f
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-13 11:19:08 +0100

    Seperate built-in converters collection

--- ABSoftware.ABSave/Converters/Converter.cs
+++ ABSoftware.ABSave/Converters/Converter.cs
@@ -13,7 +13,7 @@ using System.Text;
 
 namespace ABCo.ABSave.Converters
 {
-    public abstract class Converter : MapItem
+    public abstract class Converter<T> : MapItem
     {
         internal SaveInheritanceAttribute[]? _allInheritanceAttributes = null;
         internal Dictionary<uint, SaveInheritanceAttribute?>? _inheritanceValues = null;
@@ -28,6 +28,8 @@ namespace ABCo.ABSave.Converters
         /// </summary>
         public virtual Type[] ExactTypes { get; } = Array.Empty<Type>();
 
+        public abstract void Initialize(ref ContextGen gen);
+
         /// <summary>
         /// Attempts to generate a context.
         /// </summary>
@@ -36,51 +38,5 @@ namespace ABCo.ABSave.Converters
         public virtual bool UsesHeaderForVersion(uint version) => false;
         public abstract void Serialize(object obj, Type actualType, ref BitTarget header);
         public abstract object Deserialize(Type actualType, ref BitSource header);
-
-        internal static readonly IReadOnlyDictionary<Type, Converter> BuiltInExact = new Dictionary<Type, Converter>()
-        {
-            { typeof(byte), PrimitiveConverter.Instance },
-            { typeof(sbyte), PrimitiveConverter.Instance },
-            { typeof(char), PrimitiveConverter.Instance },
-            { typeof(ushort), PrimitiveConverter.Instance },
-            { typeof(short), PrimitiveConverter.Instance },
-            { typeof(uint), PrimitiveConverter.Instance },
-            { typeof(int), PrimitiveConverter.Instance },
-            { typeof(ulong), PrimitiveConverter.Instance },
-            { typeof(long), PrimitiveConverter.Instance },
-            { typeof(float), PrimitiveConverter.Instance },
-            { typeof(double), PrimitiveConverter.Instance },
-            { typeof(decimal), PrimitiveConverter.Instance },
-            { typeof(IntPtr), PrimitiveConverter.Instance },
-            { typeof(UIntPtr), PrimitiveConverter.Instance },
-            { typeof(bool), PrimitiveConverter.Instance },
-            { typeof(Guid), GuidConverter.Instance },
-            { typeof(string), TextConverter.Instance },
-            { typeof(StringBuilder), TextConverter.Instance },
-            { typeof(char[]), TextConverter.Instance },
-            { typeof(Version), VersionConverter.Instance },
-            { typeof(DateTime), TickBasedConverter.Instance },
-            { typeof(TimeSpan), TickBasedConverter.Instance },
-            { typeof(DictionaryEntry), KeyValueConverter.Instance },
-            { typeof(Type), TypeConverter.Instance },
-            { typeof(Assembly), AssemblyConverter.Instance },
-            { typeof(IEnumerable), EnumerableConverter.Instance },
-
-            // Based on the most common array types in .NET, remember to update in "ArrayTypeConverter" too if changing.
-            { typeof(Array), ArrayConverter.Instance },
-            { typeof(byte[]), ArrayConverter.Instance },
-            { typeof(string[]), ArrayConverter.Instance },
-            { typeof(int[]), ArrayConverter.Instance },
-        };
-
-        internal static IReadOnlyList<Converter> BuiltInNonExact = new List<Converter>()
-        {
-            KeyValueConverter.Instance,
-            AssemblyConverter.Instance,
-            TypeConverter.Instance,
-            EnumerableConverter.Instance,
-            ArrayConverter.Instance,
-            PrimitiveConverter.Instance,
-        };
     }
 }
---
+++ ABSoftware.ABSave/Helpers/BuiltInConverters.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABCo.ABSave.Helpers
+{
+    public class BuiltInConverters
+    {
+        internal static readonly IReadOnlyDictionary<Type, Converter> BuiltInExact = new Dictionary<Type, Converter>()
+        {
+            { typeof(byte), PrimitiveConverter.Instance },
+            { typeof(sbyte), PrimitiveConverter.Instance },
+            { typeof(char), PrimitiveConverter.Instance },
+            { typeof(ushort), PrimitiveConverter.Instance },
+            { typeof(short), PrimitiveConverter.Instance },
+            { typeof(uint), PrimitiveConverter.Instance },
+            { typeof(int), PrimitiveConverter.Instance },
+            { typeof(ulong), PrimitiveConverter.Instance },
+            { typeof(long), PrimitiveConverter.Instance },
+            { typeof(float), PrimitiveConverter.Instance },
+            { typeof(double), PrimitiveConverter.Instance },
+            { typeof(decimal), PrimitiveConverter.Instance },
+            { typeof(IntPtr), PrimitiveConverter.Instance },
+            { typeof(UIntPtr), PrimitiveConverter.Instance },
+            { typeof(bool), PrimitiveConverter.Instance },
+            { typeof(Guid), GuidConverter.Instance },
+            { typeof(string), TextConverter.Instance },
+            { typeof(StringBuilder), TextConverter.Instance },
+            { typeof(char[]), TextConverter.Instance },
+            { typeof(Version), VersionConverter.Instance },
+            { typeof(DateTime), TickBasedConverter.Instance },
+            { typeof(TimeSpan), TickBasedConverter.Instance },
+            { typeof(DictionaryEntry), KeyValueConverter.Instance },
+            { typeof(Type), TypeConverter.Instance },
+            { typeof(Assembly), AssemblyConverter.Instance },
+            { typeof(IEnumerable), EnumerableConverter.Instance },
+
+            // Based on the most common array types in .NET, remember to update in "ArrayTypeConverter" too if changing.
+            { typeof(Array), ArrayConverter.Instance },
+            { typeof(byte[]), ArrayConverter.Instance },
+            { typeof(string[]), ArrayConverter.Instance },
+            { typeof(int[]), ArrayConverter.Instance },
+        };
+
+        internal static IReadOnlyList<Converter> BuiltInNonExact = new List<Converter>()
+        {
+            KeyValueConverter.Instance,
+            AssemblyConverter.Instance,
+            TypeConverter.Instance,
+            EnumerableConverter.Instance,
+            ArrayConverter.Instance,
+            PrimitiveConverter.Instance,
+        };
+    }
+}
