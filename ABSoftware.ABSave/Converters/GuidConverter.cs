f8ded21f4d51a56e2b06e172966327077ac6e04e
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-13 20:09:09 +0100

    Update GuidConverter to new layout

--- ABSoftware.ABSave/Converters/GuidConverter.cs
+++ ABSoftware.ABSave/Converters/GuidConverter.cs
@@ -8,13 +8,10 @@ namespace ABCo.ABSave.Converters
 {
     public class GuidConverter : Converter
     {
-        public static GuidConverter Instance { get; } = new GuidConverter();
-        private GuidConverter() { }
-
         public override bool AlsoConvertsNonExact => false;
         public override Type[] ExactTypes { get; } = new Type[] { typeof(Guid) };
 
-        public override void Serialize(object obj, Type actualType, ConverterContext context, ref BitTarget header)
+        public override void Serialize(object obj, Type actualType, ref BitTarget header)
         {
             var guid = (Guid)obj;
             Span<byte> bytes = stackalloc byte[16];
@@ -23,17 +20,11 @@ namespace ABCo.ABSave.Converters
             header.Serializer.WriteBytes(bytes);
         }
 
-        public override object Deserialize(Type actualType, ConverterContext context, ref BitSource header)
+        public override object Deserialize(Type actualType, ref BitSource header)
         {
             Span<byte> data = stackalloc byte[16];
             header.Deserializer.ReadBytes(data);
             return new Guid(data);
-        }
-
-        public override void TryGenerateContext(ref ContextGen gen)
-        {
-            if (gen.Type == typeof(Guid)) 
-                gen.AssignContext(null, 0);
         }
     }
 }
