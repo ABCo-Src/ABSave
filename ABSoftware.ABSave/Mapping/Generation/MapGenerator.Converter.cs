7717ee41ff4c7427a06ee9e424e3a66ce817c885
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-13 14:13:42 +0100

    Add map generator logic

--- ABSoftware.ABSave/Mapping/Generation/MapGenerator.Converter.cs
+++ ABSoftware.ABSave/Mapping/Generation/MapGenerator.Converter.cs
@@ -1,4 +1,5 @@
-﻿using ABCo.ABSave.Converters;
+﻿using ABCo.ABSave.Configuration;
+using ABCo.ABSave.Converters;
 using ABCo.ABSave.Mapping.Description.Attributes;
 using System;
 using System.Collections.Generic;
@@ -10,102 +11,50 @@ namespace ABCo.ABSave.Mapping.Generation
     {
         internal MapItem? TryGenerateConverter(Type type)
         {
-            var genContext = new ContextGen(type, this);
-
-            if (!TryGetConverter(ref genContext))
-                return null;
-
-            return genContext.ContextInstance;
-        }
-
-        bool TryGetConverter(ref ContextGen gen)
-        {
-            var settings = gen.Settings;
+            var settings = Map.Settings;
 
             // Exact converter
-            if (settings.ExactConverters != null && settings.ExactConverters.TryGetValue(gen.Type, out var currentConverter))
-            {
-                currentConverter.TryGenerateContext(ref gen);
-
-                if (gen.ContextInstance == null)
-                    throw new Exception("Converter failed to provide a context for one of its exact (and by extension guaranteed) types.");
-
-                goto Found;
-            }
+            if (settings.ExactConverters.TryGetValue(type, out var currentConverter))
+                return InitializeConverter(GetConverterInstance(currentConverter), type);
 
             // Non-exact converter
             if (settings.NonExactConverters != null)
             {
                 for (int i = settings.NonExactConverters.Count - 1; i >= 0; i--)
                 {
-                    currentConverter = settings.NonExactConverters[i];
+                    var converter = GetConverterInstance(currentConverter);
 
-                    currentConverter.TryGenerateContext(ref gen);
-
-                    if (gen.ContextInstance != null)
-                        goto Found;
+                    if (converter.CheckType(type))
+                        return InitializeConverter(converter, type);
                 }
             }
 
-            return false;
-
-        Found:
-            gen.ContextInstance._converter = currentConverter;
-            gen.ContextInstance._allInheritanceAttributes = GetInheritanceAttributes(gen.Type);
-            return true;
+            return null;
         }
+
+        internal MapItem InitializeConverter(Converter converter, Type type)
+        {
+            converter.Initialize(type);
+            converter._allInheritanceAttributes = GetInheritanceAttributes(type);
+            return converter;
+        }
+
+        Converter GetConverterInstance(ConverterInfo info) => (Converter)Activator.CreateInstance(info.ConverterType);
 
         // Temporary helper until object conversion gets moved into its own converter
-        internal static SaveInheritanceAttribute? GetConverterInheritanceInfoForVersion(uint version, ConverterContext context)
+        internal static SaveInheritanceAttribute? GetConverterInheritanceInfoForVersion(uint version, Converter converter)
         {
-            // Try to get it from the dictionary.
-            if (context._inheritanceValues != null && context._inheritanceValues.TryGetValue(version, out SaveInheritanceAttribute? val))
-                return val;
+            // Try to get it from the dictionary cache.
+            SaveInheritanceAttribute? cached = converter._inheritanceValues?.GetValueOrDefault(version);
+            if (cached != null) return cached;
 
             // If it's not in there, find it in the attribute array.
-            if (context._allInheritanceAttributes == null) return null;
-            SaveInheritanceAttribute? attribute = FindInheritanceAttributeForVersion(context._allInheritanceAttributes, version);
+            if (converter._allInheritanceAttributes == null) return null;
+            SaveInheritanceAttribute? attribute = FindInheritanceAttributeForVersion(converter._allInheritanceAttributes, version);
 
-            context._inheritanceValues ??= new Dictionary<uint, SaveInheritanceAttribute?>();
-            context._inheritanceValues.Add(version, attribute);
+            converter._inheritanceValues ??= new Dictionary<uint, SaveInheritanceAttribute?>();
+            converter._inheritanceValues.Add(version, attribute);
             return attribute;
         }
-    }
-
-    public struct ContextGen
-    {
-        public Type Type;
-        internal ConverterContext? ContextInstance; // Null if not assigned by the user
-        readonly MapGenerator _gen;
-
-        public ABSaveMap Map => _gen.Map;
-        public ABSaveSettings Settings => _gen.Map.Settings;
-
-        /// <summary>
-        /// Once it has been decided the converter will convert the type, this MUST be used to provide what context will be used. 
-        /// The context may be left null in which case an instance will be filled in and provided at serialization-time with all the correct basic details.
-        /// </summary>
-        public void AssignContext(ConverterContext? contextInstance, uint maximumVersion)
-        {
-            ContextInstance = contextInstance;
-            ContextInstance ??= new ConverterContext();
-            _gen.ApplyItem(ContextInstance, Type);
-            ContextInstance.HighestVersion = maximumVersion;
-        }
-
-        /// <summary>
-        /// Gets the map for the given type.
-        /// </summary>
-        public MapItemInfo GetMap(Type type)
-        {
-            // Force converters to "AssignContext" first to get the map item out of the allocating state as quickly as possible.
-            if (ContextInstance == null)
-                throw new Exception("Converter must assign its context first before attempting to get other maps.");
-
-            return _gen.GetMap(type);
-        }
-
-        internal ContextGen(Type type, MapGenerator gen) =>
-            (Type, _gen, ContextInstance) = (type, gen, null);
     }
 }
