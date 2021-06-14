b24c54bfe2e6894e9ab01853fbbcc186f3ce2268
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-14 13:59:38 +0100

    Create attributes

---
+++ ABSoftware.ABSave/Mapping/Description/Attributes/Converters/SelectAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABCo.ABSave.Mapping.Description.Attributes.Converters
+{
+    /// <summary>
+    /// Describes a type that a converter can 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SelectAttribute : Attribute
+    {
+        public Type Type;
+        public object[] DefiniteSubTypes;
+
+        public SelectAttribute(Type type, params object[] definiteSubTypes)
+        {
+            Type = type;
+            DefiniteSubTypes = definiteSubTypes;
+        }
+    }
+}
---
+++ ABSoftware.ABSave/Mapping/Description/Attributes/Converters/SelectOtherWithCheckTypeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABCo.ABSave.Mapping.Description.Attributes.Converters
+{
+    /// <summary>
+    /// Tells ABSave to use 'CheckType' on a converter if none of the other selections match what it's searching for.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SelectOtherWithCheckTypeAttribute : Attribute { }
+}
