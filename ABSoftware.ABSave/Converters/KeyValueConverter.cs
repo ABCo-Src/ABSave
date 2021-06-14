954c84deed39d5594866d30fc837f71bfbc47499
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-14 10:24:09 +0100

    Correct KeyValueConverter

--- ABSoftware.ABSave/Converters/KeyValueConverter.cs
+++ ABSoftware.ABSave/Converters/KeyValueConverter.cs
@@ -31,62 +31,54 @@ namespace ABCo.ABSave.Converters
             serializer.SerializeItem(obj.Value, _valueMap);
         }
 
-        static void SerializeNonGeneric(DictionaryEntry obj, Context context, ABSaveSerializer serializer)
+        void SerializeNonGeneric(DictionaryEntry obj, ABSaveSerializer serializer)
         {
-            serializer.SerializeItem(obj.Key, context.KeyMap);
-            serializer.SerializeItem(obj.Value, context.ValueMap);
+            serializer.SerializeItem(obj.Key, _keyMap);
+            serializer.SerializeItem(obj.Value, _valueMap);
         }
 
-        public override object Deserialize(Type actualType, ConverterContext context, ref BitSource header)
+        public override object Deserialize(Type actualType, ref BitSource header)
         {
-            var actualContext = (Context)context;
-
-            if (actualContext.IsGeneric)
-                return DeserializeGeneric(actualType, actualContext, header.Deserializer);
+            if (_isGeneric)
+                return DeserializeGeneric(actualType, header.Deserializer);
             else
-                return DeserializeNonGeneric(header.Deserializer, actualContext);
+                return DeserializeNonGeneric(header.Deserializer);
         }
 
-        static object DeserializeGeneric(Type actualType, Context context, ABSaveDeserializer deserializer)
+        object DeserializeGeneric(Type actualType, ABSaveDeserializer deserializer)
         {
-            var key = deserializer.DeserializeItem(context.KeyMap);
-            var value = deserializer.DeserializeItem(context.ValueMap);
+            var key = deserializer.DeserializeItem(_keyMap);
+            var value = deserializer.DeserializeItem(_valueMap);
 
             return Activator.CreateInstance(actualType, key, value)!;
         }
 
-        static DictionaryEntry DeserializeNonGeneric(ABSaveDeserializer deserializer, Context context)
+        DictionaryEntry DeserializeNonGeneric(ABSaveDeserializer deserializer)
         {
-            var key = deserializer.DeserializeItem(context.KeyMap);
-            var value = deserializer.DeserializeItem(context.ValueMap);
+            var key = deserializer.DeserializeItem(_keyMap);
+            var value = deserializer.DeserializeItem(_valueMap);
 
             if (key == null) throw new NullDictionaryKeyException();
             return new DictionaryEntry(key, value);
         }
 
-        public override void TryGenerateContext(ref ContextGen gen)
+        public override void Initialize(InitializeInfo info)
         {
-            if (gen.Type.IsGenericType && gen.Type.GetGenericTypeDefinition() == typeof(KeyValuePair<,>))
+            if (_isGeneric)
             {
-                var context = new Context();
-                gen.AssignContext(context, 0);
+                var genericArgs = info.Type.GetGenericArguments();
 
-                var genericArgs = gen.Type.GetGenericArguments();
-
-                context.IsGeneric = true;
-                context.KeyMap = gen.GetMap(genericArgs[0]);
-                context.ValueMap = gen.GetMap(genericArgs[1]);
+                _keyMap = info.GetMap(genericArgs[0]);
+                _valueMap = info.GetMap(genericArgs[1]);
             }
-            else if (gen.Type == typeof(DictionaryEntry))
+            else
             {
-                var context = new Context();
-                gen.AssignContext(context, 0);
-
-                if (!gen.Settings.BypassDangerousTypeChecking) throw new DangerousTypeException("a general 'DictionaryEntry' type that could contain any type of element.");
-                context.IsGeneric = false;
-                context.KeyMap = context.ValueMap = gen.GetMap(typeof(object));
+                _keyMap = _valueMap = info.GetMap(typeof(object));
             }
         }
+
+        public override bool CheckType(CheckTypeInfo info) =>
+            _isGeneric = info.Type.IsGenericType && info.Type.GetGenericTypeDefinition() == typeof(KeyValuePair<,>);
 
         public override bool AlsoConvertsNonExact => true;
         public override Type[] ExactTypes { get; } = new Type[]
