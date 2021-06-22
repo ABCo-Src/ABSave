ccc23d575706e7875c0561e7b651d75d8d087879
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-22 07:37:19 +0100

    Organise intermediate processing into static classes

---
+++ ABSoftware.ABSave/Mapping/Generation/IntermediateObject/IntermediateMapper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABCo.ABSave.Mapping.Generation.IntermediateObject
+{
+    // This is created first before an object map is made. This gathers the raw data from either the
+    // attributes or externally defined maps, and then translate it all, alongside a lot of analysis data, into an
+    // intermediary form, that can then used to make the final maps for all the different version numbers.
+    internal static class IntermediateMapper
+    {
+        // Context used while creating intermediate info.
+        internal static TranslationContext _intermediateContext = new TranslationContext();
+
+        /// <summary>
+        /// Clears the list <see cref="MapGenerator.CurrentObjMembers"/> and inserts all of the members and information attached to them.
+        /// </summary>
+        internal uint CreateIntermediateObjectInfo(Type type, ref ObjectIntermediateInfo info)
+        {
+            _intermediateContext = new TranslationContext(type);
+
+            // Coming soon: Settings-based mapping
+            var members = IntermediateReflectionInfo.FillInfo(out SaveInheritanceAttribute[]? attr);
+
+            if (_intermediateContext.TranslationCurrentOrderInfo == -1)
+                Array.Sort(members);
+
+            info = new ObjectIntermediateInfo(members, attr);
+            return _intermediateContext.HighestVersion;
+        }
+
+        internal class ReflectionMapper
+        {
+            internal MapGenerator Gen;
+            internal ReflectionMapper(MapGenerator gen)
+            {
+                Gen = gen;
+                _threadPoolAddItem = ProcessAttributesOnThreadPool;
+            }
+
+
+        }
+
+        internal static void FillMainInfo(ObjectIntermediateItem newItem, int order, int startVer, int endVer)
+        {
+            newItem.Order = order;
+
+            // If the version given is -1, that means it doesn't have a set end, so we'll just fill that in with "uint.MaxValue".            
+            newItem.StartVer = checked((uint)startVer);
+            newItem.EndVer = endVer == -1 ? uint.MaxValue : checked((uint)endVer);
+        }
+
+        internal void UpdateContextFromItem(ObjectIntermediateItem newItem)
+        {
+            // Check ordering
+            if (_intermediateContext.TranslationCurrentOrderInfo != -1)
+            {
+                if (newItem.Order >= _intermediateContext.TranslationCurrentOrderInfo)
+                    _intermediateContext.TranslationCurrentOrderInfo = newItem.Order;
+                else
+                    _intermediateContext.TranslationCurrentOrderInfo = -1;
+            }
+
+            UpdateVersionInfo(newItem.StartVer, newItem.EndVer);
+        }
+
+        void UpdateVersionInfo(uint startVer, uint endVer)
+        {
+            // If there is no upper we'll only update the highest version based on what the minimum is.
+            if (endVer == uint.MaxValue)
+            {
+                if (startVer > _intermediateContext.HighestVersion)
+                    _intermediateContext.HighestVersion = startVer;
+            }
+
+            // If not update based on what their custom high is.
+            else
+            {
+                if (endVer > _intermediateContext.HighestVersion)
+                    _intermediateContext.HighestVersion = endVer;
+            }
+        }
+
+        internal struct TranslationContext
+        {
+            public Type ClassType;
+            public int TranslationCurrentOrderInfo;
+
+            // Used to count how many unskipped members were present so we know the size for our final array.
+            public int UnskippedMemberCount;
+            public uint HighestVersion;
+
+            public TranslationContext(Type classType)
+            {
+                ClassType = classType;
+                TranslationCurrentOrderInfo = 0;
+                UnskippedMemberCount = 0;
+                HighestVersion = 0;
+            }
+        }
+
+        internal struct TranslationContext
+        {
+            public Type ClassType;
+            public int TranslationCurrentOrderInfo;
+
+            // Used to count how many unskipped members were present so we know the size for our final array.
+            public int UnskippedMemberCount;
+            public uint HighestVersion;
+
+            public TranslationContext(Type classType)
+            {
+                ClassType = classType;
+                TranslationCurrentOrderInfo = 0;
+                UnskippedMemberCount = 0;
+                HighestVersion = 0;
+            }
+        }
+    }
+}
---
+++ ABSoftware.ABSave/Mapping/Generation/IntermediateObject/IntermediateReflectionMapper.cs
@@ -0,0 +1,124 @@
+﻿using ABCo.ABSave.Exceptions;
+using ABCo.ABSave.Mapping.Description;
+using ABCo.ABSave.Mapping.Description.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ABCo.ABSave.Mapping.Generation.Object
+{
+    /// <summary>
+    /// A class responsible for creating intermediate info about an object via reflection.
+    /// </summary>
+    internal static class IntermediateReflectionMapper
+    {
+        public ObjectIntermediateItem[] FillInfo(out SaveInheritanceAttribute[]? inheritanceInfo)
+        {
+            var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+            var classType = Gen._intermediateContext.ClassType;
+            var mode = GetClassInfo(classType, out inheritanceInfo);
+
+            // Get the members
+            FieldInfo[] _currentFields = GetFields(bindingFlags, classType, mode);
+            PropertyInfo[] _currentProperties = GetProperties(bindingFlags, classType, mode);
+
+            PrepareBuffer(_currentFields.Length + _currentProperties.Length);
+
+            // Process the members
+            return ProcessMembers();
+        }
+
+        static void ProcessMembers(MemberInfo[] members, List<ObjectIntermediateItem> dest)
+        {
+            for (int i = 0; i < members.Length; i++)
+                dest.Add(GetItemForMember(members[i]));
+        }
+
+        internal static ObjectIntermediateItem? GetItemForMember(MemberInfo info)
+        {
+            // Get the attributes - skip this item if there are none
+            var attributes = info.GetCustomAttributes(typeof(MapAttr), false);
+            if (attributes.Length == 0) return null;
+
+            var newItem = new ObjectIntermediateItem();
+
+            // Create the item.
+            bool successful = FillItemFromAttributes(newItem, info, attributes);
+            if (!successful) throw new IncompleteDetailsException(info);
+
+            dest = newItem;
+            count++;
+        }
+
+        private static bool FillItemFromAttributes(ObjectIntermediateItem dest, MemberInfo info, object[] attributes)
+        {
+            newItem.Details.Unprocessed = info;
+
+            bool loadedSaveAttribute = false;
+            for (int i = 0; i < attributes.Length; i++)
+            {
+                switch (attributes[i])
+                {
+                    case SaveAttribute save:
+                        FillMainInfo(newItem, save.Order, save.FromVer, save.ToVer);
+                        loadedSaveAttribute = true;
+                        break;
+                }
+            }
+
+            return loadedSaveAttribute;
+        }
+
+        SaveMembersMode GetClassInfo(Type classType, out SaveInheritanceAttribute[]? inheritanceInfo)
+        {
+            // TODO: This is just to temporarily support "object" until proper settings mapping comes in.
+            SaveMembersMode res = GetClassMode(classType);
+
+            inheritanceInfo = (SaveInheritanceAttribute[])classType.GetCustomAttributes<SaveInheritanceAttribute>(false);
+
+            if (inheritanceInfo.Length == 0)
+                inheritanceInfo = null;
+            else
+            {
+                for (int i = 0; i < inheritanceInfo.Length; i++)
+                {
+                    var info = inheritanceInfo[i];
+                    Gen.UpdateVersionInfo(info.FromVer, info.ToVer);
+                }
+            }
+
+            return res;
+        }
+
+        private static SaveMembersMode GetClassMode(Type classType)
+        {
+            if (classType == typeof(object)) return SaveMembersMode.Fields;
+
+            var attribute = classType.GetCustomAttribute<SaveMembersAttribute>(false);
+            if (attribute == null) throw new UnserializableType(classType);
+
+            return attribute.Mode;
+        }
+
+        static FieldInfo[] GetFields(BindingFlags bindingFlags, Type classType, SaveMembersMode mode)
+        {
+            var fields = Array.Empty<FieldInfo>();
+
+            if ((mode & SaveMembersMode.Fields) > 0)
+                fields = classType.GetFields(bindingFlags);
+
+            return fields;
+        }
+
+        static PropertyInfo[] GetProperties(BindingFlags bindingFlags, Type classType, SaveMembersMode mode)
+        {
+            var properties = Array.Empty<PropertyInfo>();
+
+            if ((mode & SaveMembersMode.Properties) > 0)
+                properties = classType.GetProperties(bindingFlags);
+
+            return properties;
+        }
+    }
+}
