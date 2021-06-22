e89d02ed6fb19431af3af066ad106765c1984fc6
Alex-ABWorld <alexbu2015@hotmail.com>
2021-06-22 12:39:26 +0100

    Add ObjectConverter

---
+++ ABSoftware.ABSave/Converters/ObjectConverter.cs
@@ -0,0 +1,24 @@
+﻿using ABCo.ABSave.Deserialization;
+using ABCo.ABSave.Mapping.Generation;
+using ABCo.ABSave.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABCo.ABSave.Converters
+{
+    public class ObjectConverter : Converter
+    {
+        internal ObjectIntermediateItem[]? _rawMembers;
+
+        public override void Serialize(in SerializeInfo info, ref BitTarget header)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Deserialize(in DeserializeInfo info, ref BitSource header)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
