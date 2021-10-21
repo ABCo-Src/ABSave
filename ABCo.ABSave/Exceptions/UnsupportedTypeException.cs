55e110ce697ad92c86d77b25c3a96f10aeaf7cd1
Jeremy <jenkenkur@gmail.com>
2021-10-22 01:09:20 +0200

    Add UnsupportedTypeException

---
+++ ABCo.ABSave/Exceptions/UnsupportedTypeException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ABCo.ABSave.Exceptions
+{
+	public class UnsupportedTypeException : ABSaveException
+	{
+		public UnsupportedTypeException(Type type, string reason) : base($"The supplied type '{type.Name}' is not supported. Reason: {reason}")
+		{ }
+	}
+}
---
+++ ABCo.ABSave/Helpers/TypeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABCo.ABSave.Helpers
+{
+	public static class TypeExtensions
+	{
+		public static bool HasEmptyOrDefaultConstructor(this Type type)
+		{
+			return type.IsValueType || type.IsAbstract || type.GetConstructor(Type.EmptyTypes) != null;
+		}
+	}
+}
